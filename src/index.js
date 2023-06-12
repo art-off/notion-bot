ca17cdec30ba35129909c819858b5ba8787e87ac
Artem Rylov <tema2707@icloud.com>
2023-06-12 11:16:47 +0300

    voices works

--- src/index.js
+++ src/index.js
@@ -4,21 +4,24 @@ import { Repository } from "./database/reposiroty.js";
 import { InMemoryStorage } from "./in-memory/in-memory-storage.js";
 import { makeBot } from "./telegram-bot/bot.js";
 import { makeNotionClient } from "./notion/notion-client.js"
-import { VoiceFileManager } from './voice/voice-file.js';
+import { SpeechToTextTranslator } from './speech-to-text/speech-to-text-translator.js';
 
 // load ENV from .env
 dotenv.config()
 
+const getEnvOrFail = (name) => process.env[name] || assert.fail(`${name} must be in env vars`)
+
 // Create dependencies
 const inMemoryStorage = new InMemoryStorage()
 const repository = new Repository(
-    process.env.SQLITE_FILE_PATH || assert.fail("SQLITE_FILE_PATH must be in env vars")
+    getEnvOrFail('SQLITE_FILE_PATH')
 ).prepared()
-const voiceFileManager = new VoiceFileManager(
-    process.env.VOICES_FOLDER_PATH || assert.fail("VOICES_FOLDER_PATH must be in evn vars")
+const speechToTextTranslator = new SpeechToTextTranslator(
+    getEnvOrFail('YANDEX_API_KEY'),
+    getEnvOrFail('YANDEX_FORLDER_ID')
 )
 
-const bot = makeBot(repository, inMemoryStorage, makeNotionClient, voiceFileManager)
+const bot = makeBot(repository, inMemoryStorage, makeNotionClient, speechToTextTranslator)
 bot.launch();
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
---
+++ src/speech-to-text/speech-to-text-translator.js
@@ -0,0 +1,38 @@
+import axios from "axios"
+
+export class SpeechToTextTranslator {
+    constructor(yandexApiKey, yandexFolderId) {
+        this.yandexApiKey = yandexApiKey
+        this.yandexFolderId = yandexFolderId
+    }
+
+    async speechToText(voiceData) {
+        let iAmToken = await this.getIAmToken()
+        if (!iAmToken) {
+            console.log('iAmToken error')
+            return null
+        }
+        let response = await axios.post(
+            'https://stt.api.cloud.yandex.net/speech/v1/stt:recognize',
+            Buffer.from(voiceData),
+            {
+                params: {
+                    folderId: this.yandexFolderId,
+                    lang: 'ru-RU'
+                },
+                headers: {
+                    'Authorization': `Bearer ${iAmToken}`
+                }
+            }
+        )
+
+        return response['data']['result']
+    }
+
+    async getIAmToken() {
+        let response = await axios.post('https://iam.api.cloud.yandex.net/iam/v1/tokens', {
+            yandexPassportOauthToken: this.yandexApiKey
+        })
+        return response['data']['iamToken']
+    }
+}
